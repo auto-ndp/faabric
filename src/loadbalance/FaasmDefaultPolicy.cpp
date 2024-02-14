5c914d2da35c74d2c12679fd5b3cb286dad28123
Donald Jennings <D.D.Jennings@outlook.com>
2024-02-14 23:46:05 +0000

    fixed wrong method call

--- src/loadbalance/FaasmDefaultPolicy.cpp
+++ src/loadbalance/FaasmDefaultPolicy.cpp
@@ -7,7 +7,7 @@ std::vector<std::string> FaasmDefaultPolicy::dispatch(const std::set<std::string
     hosts_delta.reserve(number_of_messages);
     for (auto& faaslet : warm_faaslets) {
         int remainder = number_of_messages;
-        faabric::HostResources host_resources = scheduler.getResourcesForHost(faaslet);
+        faabric::HostResources host_resources = scheduler.getHostResources(faaslet);
         int availableSlots = host_resources.slots() - host_resources.usedslots();
         availableSlots = std::max(availableSlots, 0);
         int nOnThisHost = std::min<int>(availableSlots, 1);
